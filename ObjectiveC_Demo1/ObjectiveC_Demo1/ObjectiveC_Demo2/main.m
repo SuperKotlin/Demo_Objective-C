6b90b4a5292a64d5d885464d092a6bca56c522f8
99305919@qq.com <ZXCVBNM.12>
2020-12-01 22:38:38 +0800

    commit

---
+++ ObjectiveC_Demo1/ObjectiveC_Demo1/ObjectiveC_Demo2/main.m
@@ -0,0 +1,9 @@
+//
+//  main.m
+//  ObjectiveC_Demo1
+//
+//  Created by 朱勇 on 2020/12/1.
+//  Copyright © 2020 朱勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ObjectiveC_Demo1/ObjectiveC_Demo1/demo2.m
@@ -0,0 +1,16 @@
+//
+//  demo2.m
+//  ObjectiveC_Demo1
+//
+//  Created by 朱勇 on 2020/12/1.
+//  Copyright © 2020 朱勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(){
+    
+    NSLog(@"NULL和nil的区别...");
+    
+    return 0;
+}
