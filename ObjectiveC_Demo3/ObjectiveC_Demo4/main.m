5a7b17635fadfc03f53e04c29f33364ab6808aa6
99305919@qq.com <ZXCVBNM.12>
2020-12-01 23:46:16 +0800

    commit

--- ObjectiveC_Demo3/ObjectiveC_Demo4/main.m
+++ ObjectiveC_Demo3/ObjectiveC_Demo4/main.m
@@ -1,25 +1,12 @@
 //
 //  main.m
-//  ObjectiveC_Demo3
+//  ObjectiveC_Demo4
 //
 //  Created by 朱勇 on 2020/12/1.
 //  Copyright © 2020 朱勇. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
-@interface Dog : NSObject{
-    
-}
-
-@end
-
-@implementation Dog
- 
-
-@end
-
-#pragma mark -
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
