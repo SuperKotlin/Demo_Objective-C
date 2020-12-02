4dc217a2209983cc735a14cacb10190e20bb5467
99305919@qq.com <ZXCVBNM.12>
2020-12-02 12:33:00 +0800

    commit

--- ObjectiveC_Demo/ObjectiveC_Demo4/main.m
+++ ObjectiveC_Demo/ObjectiveC_Demo4/main.m
@@ -6,12 +6,42 @@
 //  Copyright © 2020 朱勇. All rights reserved.
 //
 
+/*
+ 
+ 1、属性不允许声明的时候初始化赋值。
+    @interface Person : NSObject{
+    //    NSString *_name1 = @"jack"; //这是错误写法，属性不允许声明的时候初始化赋值。
+    }
+ 
+ 2、
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
 #import <Foundation/Foundation.h>
 
+@interface Person : NSObject{
+    @public
+//    NSString *_name1 = @"jack"; //这是错误写法，属性不允许声明的时候初始化赋值。
+    NSString *_name2;
+}
+
+@end
+
+@implementation Person
+ 
+
+@end
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    Person *person = [Person new];
+    person->_name = @"Jane";
+    NSLog(@"%@",person->_name);
+    
     return 0;
 }
